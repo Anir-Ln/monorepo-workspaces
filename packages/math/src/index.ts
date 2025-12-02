2fe44a87682cd96efb1419942787451361a3588a
AnirLn <anirlahyane@gmail.com>
2025-12-02 22:57:20 +0100

    feat: add sum func

--- packages/math/src/index.ts
+++ packages/math/src/index.ts
@@ -5,3 +5,7 @@ export const add = (x: number, y: number) => {
 export const multiply = (x: number, y: number) => {
   return x * y;
 }
+
+export const sum = (arr: number[]) : number => {
+  return arr.reduce((prev, curr) => prev + curr, 0);
+}
